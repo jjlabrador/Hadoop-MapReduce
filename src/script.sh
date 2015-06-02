50e47cb6807bebdb00fd4f08cc743899f112aae2
Juan José <jjlabradorglez@gmail.com>
2015-06-02 01:11:55 +0100

    Improved script.sh and added output file (part-r-00000)

--- src/script.sh
+++ src/script.sh
@@ -6,3 +6,5 @@ hadoop fs -rmdir /user/hduser/output
 hadoop fs -rm /user/hduser/input/*
 hadoop fs -copyFromLocal playas.csv /user/hduser/input
 ../bin/hadoop jar pr.jar PlayaRest /user/hduser/input /user/hduser/output -files /opt/hadoop/jj/restauracion.csv -D 5
+hadoop fs -cat /user/hduser/output/part-r-00000 | more
+hadoop fs -copyToLocal /user/hduser/output/part-r-00000 .
